1c1666ffd2d2ba6bb3d8d0fc884dd60a7713d313
fatima-ca <fatimacastilloag@gmail.com>
2025-03-10 07:42:04 -0600

    cambie a image y agregue dos propiedades2

--- src/app/componentes/Userinfobox.tsx
+++ src/app/componentes/Userinfobox.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 
 interface UserInfoProps {
@@ -57,7 +58,8 @@ export default function Userinfobox({ name, last, email, picture, birthday, phon
   return (
     <main className="">
       <div className="flex flex-col justify-center items-center gap-4 relative w-170 h-96 bg-gray-300 rounded-xl shadow-lg p-4">
-        <img src={picture} alt="User" className="rounded-full w-25 h-25 mb-2" />
+        <img src={picture} alt="User" className="rounded-full w-25 h-25 mb-2" width={70}
+      height={70}/>
 
         <h1 className="text-sm text-gray-700">{getDesc()}</h1>
         <h5 className="text-lg font-bold">{getData()}</h5>
